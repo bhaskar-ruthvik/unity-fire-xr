8931131071ed193f386d23efe39ed2ad71794f8a
abhinav <abhinavyekkali@gmail.com>
2025-03-25 12:25:00 +0530

    Added fire extinguisher sound

--- Assets/Extinguisher.cs
+++ Assets/Extinguisher.cs
@@ -4,6 +4,9 @@ public class Extinguisher : MonoBehaviour
 {
     [SerializeField] GameObject obj;
     [SerializeField] private float amountExtinguishedPerSecond = 0.5f;
+    [SerializeField] private AudioSource extinguisherSound; 
+
+    private bool isPlayingSound = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +18,18 @@ public class Extinguisher : MonoBehaviour
     {
         if(obj.activeInHierarchy && Physics.Raycast(obj.transform.position, obj.transform.forward, out RaycastHit hit,100f) && hit.collider.TryGetComponent(out Fire fire)){
             fire.TryExtinguish(amountExtinguishedPerSecond * Time.deltaTime);
+        
+
+            if(!isPlayingSound && extinguisherSound!=null){ // Play sound if not already playing
+                extinguisherSound.Play();
+                isPlayingSound=true;
+            }
+        }
+        else{
+            if(isPlayingSound && extinguisherSound!=null){ //Stop sound when not in use
+                extinguisherSound.Stop();
+                isPlayingSound=false;
+            }
         }
     }
 }
