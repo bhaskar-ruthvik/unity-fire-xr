c0bbb54a62ce810a60f803022e87dadfd262d9b0
Bhaskar Ruthvik <ruthvikcoool@gmail.com>
2025-04-23 12:17:33 +0530

    Fixed Healthbar Position and Added Timer

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -7,13 +7,19 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] Slider bar;
     [SerializeField] TextMeshProUGUI text;
-    
+    [SerializeField] TextMeshProUGUI timer;
     [SerializeField] GameObject canvas;
+    float startTime;
+    float time;
+    int minutes;
+    float seconds;
     [SerializeField] float healthLossRate = 0.2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         text.text = "Health: "+Math.Round(bar.normalizedValue*100).ToString()+"/100";
+        startTime = Time.time;
+        timer.text = "Elapsed: 0s";
     }
 
     // Update is called once per frame
@@ -21,7 +27,10 @@ public class HealthBar : MonoBehaviour
     {
         bar.normalizedValue -= 1e-3f *Time.deltaTime * healthLossRate;
         text.text = "Health: "+Math.Round(bar.normalizedValue*100).ToString()+"/100";
-
+        time  = Time.time - startTime;
+        minutes = (int) time/60;
+        seconds = time%60;
+        timer.text = "Elapsed: "+minutes.ToString()+"min "+Math.Round(seconds).ToString()+"s";
         if(bar.normalizedValue==0){
             canvas.SetActive(true);
         }
